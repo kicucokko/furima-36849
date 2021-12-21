7be3f55d99d33be11fb6b222e17a439f003957df
kicucokko <91193214+kicucokko@users.noreply.github.com>
2021-12-22 00:25:06 +0900

    カラム作成とユーザー登録画面構築
    ・usersテーブルの作成とバリテーションを記述
    ・ユーザー登録画面表示のためのcontroller/viewsへの記述
    ・新規登録画面表示のための部分テンプレートの挿入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :birth_date])
+  end
 end
+
