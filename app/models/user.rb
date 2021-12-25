64ab1312a540e5e91f0f4cf16d91f22547b5d1ad
kicucokko <91193214+kicucokko@users.noreply.github.com>
2021-12-25 15:12:22 +0900

    エラーメッセージの設定・バリデーションの記述
    ・エラーメッセージテンプレートの挿入
    ・新規登録画面について
    　パスワードが6文字以上
    　名前は全角文字であること
    　カナは全角カタカナであること
    以上のバリデーション記述
    ・バリデーションに伴うエラーメッセージの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_kana, presence: true
-  validates :first_kana, presence: true
   validates :birth_date, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,message: "Input full-width characters"} do
+    validates :first_name
+    validates :last_name
+  end
 
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ ,message: "Input full-width katakana characters"} do
+    validates :last_kana
+    validates :first_kana
+  end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX,message:"Include both letters and numbers"
 end
