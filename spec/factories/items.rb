cf933e9015744c53ed77f6478c2c8c23e03ed054
kicucokko <91193214+kicucokko@users.noreply.github.com>
2022-01-08 14:35:35 +0900

    テスト機能の実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name          {Faker::Lorem.sentence} 
+    item_description   {Faker::Lorem.sentence} 
+    category_id        {2}
+    quality_id         {2}
+    fee_id             {2}
+    prefecture_id      {2}
+    before_shipping_id {2}
+    price              {400}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
