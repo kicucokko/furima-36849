12682ef16731fdade1f49c4af266d0bd10885b3e
kicucokko <91193214+kicucokko@users.noreply.github.com>
2022-01-08 13:51:34 +0900

    出品入力機能の実装
    ・new/createのルーティング記述/viewの挿入
    ・画像登録についての補足作業

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,24 @@
 class ItemsController < ApplicationController
 
- def index
- end
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+     redirect_to root_path
+    else
+     render :new
+    end
+  end
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :item_description, :category_id, :quality_id, :fee_id, :prefecture_id, :before_shipping_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
