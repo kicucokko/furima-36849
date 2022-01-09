2a2ed6367a3bca50838779eb4ded8e527967c910
kicucokko <91193214+kicucokko@users.noreply.github.com>
2022-01-09 23:41:42 +0900

    Formオブジェクトの実装
    ・Purchasescontrollerへの記述
    ・アソシエーションの記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,16 @@
 class PurchasesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @purchase_receiver = PurchaseReceiver.new 
   end
   def create
+    @item = Item.find(params[:item_id])
+    @purchase_receiver  = PurchaseReceiver.new(purchase_receiver_params)
+  end
+
+  private
+
+  def purchase_receiver_params 
+    params.require(:purchase_receiver).permit(:zip, :prefecture_id, :city, :block, :build, :phone).merge(user_id: current_user.id,item_id: params[:item_id])
   end
 end
