1602a2081bc9ad2289fb1b78535a3067fff33b7b
kicucokko <91193214+kicucokko@users.noreply.github.com>
2022-01-09 14:57:56 +0900

    beforeaction の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index,:show]
+  before_action :move_to_sign_in, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @items = Item.all.order("items.created_at DESC")
   end
@@ -36,6 +38,17 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:item_name, :item_description, :category_id, :quality_id, :fee_id, :prefecture_id, :before_shipping_id, :price, :image).merge(user_id: current_user.id)
+  end
+  def move_to_sign_in
+    unless user_signed_in?
+      redirect_to action: "new_user_session_path"
+    end
+  end
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
   end
 
 end
