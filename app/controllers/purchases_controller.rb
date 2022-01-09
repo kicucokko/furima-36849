68a328fc14f97224bec26c14c97cd1d777a64238
kicucokko <91193214+kicucokko@users.noreply.github.com>
2022-01-10 00:10:04 +0900

    クレジットカード決済機能実装
    ・tokenの追加記述
    ・controller/Viewのクレジットカード実装に関わるアクション/カラム名の記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,6 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: :index
+
   def index
     @item = Item.find(params[:item_id])
     @purchase_receiver = PurchaseReceiver.new 
@@ -6,11 +8,27 @@ class PurchasesController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @purchase_receiver  = PurchaseReceiver.new(purchase_receiver_params)
+    if @purchase_receiver.valid?
+      pay_item
+     @purchase_receiver.save
+     redirect_to root_path
+   else
+     render 'index'
+   end
   end
 
   private
 
   def purchase_receiver_params 
-    params.require(:purchase_receiver).permit(:zip, :prefecture_id, :city, :block, :build, :phone).merge(user_id: current_user.id,item_id: params[:item_id])
+    params.require(:purchase_receiver).permit(:zip, :prefecture_id, :city, :block, :build, :phone).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount:@item.price,
+      card: params[:token],
+      currency:'jpy'
+    )
   end
 end
--- app/models/purchase_receiver.rb
+++ app/models/purchase_receiver.rb
@@ -1,10 +1,11 @@
 class PurchaseReceiver
   include ActiveModel::Model
-  attr_accessor :zip, :prefecture_id, :city, :block, :build, :phone,  :user_id, :item_id
+  attr_accessor :zip, :prefecture_id, :city, :block, :build, :phone,  :user_id, :item_id,:token
 
   with_options presence: true do
     validates :user_id
     validates :item_id
+    validates :token
     validates :zip, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :city
